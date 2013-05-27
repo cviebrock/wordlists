c8a6edaa21dbf852e612b4eae7f15b0980a14daf
Colin Viebrock <colin@viebrock.ca>
2013-05-27 12:27:06 -0600

    initial models

---
+++ app/models/BaseWordModel.php
@@ -0,0 +1,29 @@
+<?php
+
+class BaseWordModel extends Eloquent {
+
+	public static $letters = array(
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+	);
+
+
+	public static function createNew( $word )
+	{
+
+		$attributes = array(
+			'word' => $word,
+			'length' => Str::length( $word ),
+			'alphagram' => Wordlist::alphagram( $word)
+		);
+
+		$frequency = Wordlist::letterFrequency( $word );
+
+		$attributes = array_merge( $attributes, $frequency );
+
+		print_r($attributes);
+
+	}
+
+
+
+}
---
+++ app/models/Word.php
@@ -0,0 +1,8 @@
+<?php
+
+class Word extends BaseWordModel {
+
+
+
+
+}
