afce0996066bfc5854ef97945cde46a0c38bb9b0
Colin Viebrock <colin@viebrock.ca>
2013-05-27 12:26:28 -0600

    helper class

---
+++ app/libraries/Wordlist.php
@@ -0,0 +1,65 @@
+<?php
+
+
+class Wordlist {
+
+	/**
+	 * Returns an alphagram (letters sorted alphabetically)
+	 * of a given word.
+	 *
+	 * e.g. WORDLIST -> DILORSTW
+	 *
+	 * @param  string $word
+	 * @return string
+	 */
+	public static function alphagram( $word )
+	{
+		return join('', static::wordToArray($word));
+	}
+
+
+	/**
+	 * Returns an array of letter count for a word.
+	 *
+	 * e.g. SCRABBLE -> array ( 'a'=>1, 'b'=>2, 'c'=>1, 'e'=>1, 'l'=>1, 'r'=>1, 's'=>1 )
+	 *
+	 * @param  string $word
+	 * @return array
+	 */
+	public static function letterFrequency( $word )
+	{
+		$array = array_flip( static::wordToArray( $word ) );
+
+		$i = 0;
+
+		foreach ($array as $letter => $n ) {
+			if ($i++==0) {
+				$array[$letter] = $n+1;
+			} else {
+				$array[$letter] = $n - $prev_n;
+			}
+			$prev_n = $n;
+		}
+
+		return $array;
+
+	}
+
+
+
+	/**
+	 * Convert a word into an array of it's letters.
+	 *
+	 * @param  string $word
+	 * @return array
+	 */
+	private static function wordToArray( $word )
+	{
+		$array = str_split($word);
+		sort($array);
+		return $array;
+	}
+
+
+
+}
