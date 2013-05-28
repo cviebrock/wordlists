bd7f88e11a269a5bdfbe23e905f26ae57de3ff01
Colin Viebrock <colin@viebrock.ca>
2013-05-28 13:12:06 -0500

    fix base model

--- app/models/BaseWordModel.php
+++ app/models/BaseWordModel.php
@@ -2,6 +2,10 @@
 
 class BaseWordModel extends Eloquent {
 
+	protected $guarded = array('id');
+
+	public $timestamps = false;
+
 	public static $letters = array(
 		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 	);
@@ -13,14 +17,14 @@ class BaseWordModel extends Eloquent {
 		$attributes = array(
 			'word'      => $word,
 			'length'    => Str::length( $word ),
-			'alphagram' => Wordlist::alphagram( $word)
+			'alphagram' => Wordlist::alphagram( $word )
 		);
 
 		$frequency = Wordlist::letterFrequency( $word );
 
 		$attributes = array_merge( $attributes, $frequency );
 
-		print_r($attributes);
+		return new static( $attributes );
 
 	}
 
